a316cc11b1e16f999342fa58a4810f446d0b26c2
ABR <abr@gmail.com>
2023-11-01 09:03:20 +0500

    Ch#8 Lecture5 use the data in the function as well

--- ch8/src/components/UserDetail.vue
+++ ch8/src/components/UserDetail.vue
@@ -2,13 +2,18 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p> User name : {{ name }}</p>
+        <p> User name : {{ switch_name() }}</p>
     </div>
 </template>
 
 <script>
 export default {
-  props: ['name']
+  props: ['name'],
+  methods: {
+    switch_name() {
+      return this.name.split("").reverse().join("");
+    }
+  }
 }
 </script>
 
