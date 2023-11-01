314982378826baf51b5cd3fed85cb4f2c2831b5d
ABR <abr@gmail.com>
2023-11-01 08:38:14 +0500

    8#2 Leave with Questino how to pass data between parrent and child componnets

--- ch8/src/components/User.vue
+++ ch8/src/components/User.vue
@@ -3,6 +3,7 @@
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
         <hr>
+      <button @click="nameChange">change my name </button>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
                 <app-user-detail></app-user-detail>
@@ -19,10 +20,20 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+      data: function (){
+        return {
+          change: 'ABR'
+        };
+      },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
+        },
+      methods:{
+        nameChange(){
+          this.name = "Max"
         }
+      }
     }
 </script>
 
--- ch8/src/components/UserDetail.vue
+++ ch8/src/components/UserDetail.vue
@@ -2,6 +2,7 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p> User name : {{ name }}</p>
     </div>
 </template>
 
